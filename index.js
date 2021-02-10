a1c9678a3b27a744f124dab5557fa3b79b6a5251
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-10 12:22:57 -0500

    commit tree df16a23d29c4c2834755b12ef48170b775bea131

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z\ \'\-]/gm, "");
+  }
+  static titleize(string) {
+    const arr = string.split(" ");
+    let answer = `${this.capitalize(arr[0])}`;
+    const smallWordArr = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    arr.forEach((word, i) => {
+      if (i === 0) {
+        return;
+      } else if (smallWordArr.includes(word)) {
+        answer += ` ${word}`;
+      } else {
+        answer += ` ${this.capitalize(word)}`;
+      }
+    });
+    return answer;
+  }
 }
